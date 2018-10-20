e5afd4866136311055d3194fd1622ee04f6125a3
luojinrong <840841939@qq.com>
2018-10-20 17:17:54 +0800

    Site updated: 2018-10-20 17:17:53

--- js/gitment.browser.js
+++ js/gitment.browser.js
@@ -3360,7 +3360,7 @@ var Gitment = function () {
     this.useTheme(_default2.default);
 
     Object.assign(this, {
-      id: <%=page.title%>,
+      id: page.title,
       title: window.document.title,
       link: window.location.href,
       desc: '',
